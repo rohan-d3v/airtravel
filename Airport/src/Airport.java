0ca6abc24158727509aec30a097406ea91dcc650
Rohan <rxr353@case.edu>
2019-09-07 15:21:24 -0400

    Set up Base Files

--- Airport/src/Airport.java
+++ Airport/src/Airport.java
@@ -3,7 +3,10 @@ import java.time.Duration;
 //rohan and caitlin
 public  final class Airport {
     private Airport(String code, Duration connectionTimeMin){
-
+        String AirportCode=null;
+        String
+        code = AirportCode;
+        connectionTimeMin =
     }
 
 }
