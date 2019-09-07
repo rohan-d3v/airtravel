6fe8fd85de99284cbccb8a82cfcec8990aa6f3de
Caitlin Campbell <cac226@case.edu>
2019-09-07 16:24:14 -0400

    merge

--- Airport/src/AbstractFlight.java
+++ Airport/src/AbstractFlight.java
@@ -1,3 +1,30 @@
-public abstract class AbstractFlight implements Flight {
+/**
+ * @names:      Caitlin Campbell, Rohan Krishna Ramkhumar
+ * @case-id:    cac226, rxr353
+ * @project:    2) AirTravel
+ * @class:      AbstractFlight
+ */
+import java.time.LocalTime;
+import java.time.Duration;
 
+public abstract class AbstractFlight implements Flight {
+    public final Airport origin() {
+        return getLeg().origin();
+    }
+
+    public final Airport destination() {
+        return getLeg().destination();
+    }
+
+    public final LocalTime departureTime() {
+        return getFlightSchedule().departureTime();
+    }
+
+    public final LocalTime arrivalTime() {
+        return getFlightSchedule().arrivalTime();
+    }
+
+    public final boolean isShort(Duration durationMax) {
+        return getFlightSchedule().isShort(durationMax);
+    }
 }
