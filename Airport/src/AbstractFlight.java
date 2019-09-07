3ef01abadd53766f56b20d2cc09ba322caec23cb
Caitlin Campbell <cac226@case.edu>
2019-09-07 16:19:34 -0400

    updated abstract flight, fixed related issues

--- Airport/src/AbstractFlight.java
+++ Airport/src/AbstractFlight.java
@@ -1,3 +1,25 @@
-public abstract class AbstractFlight implements Flight {
 
+import java.time.LocalTime;
+import java.time.Duration;
+
+public abstract class AbstractFlight implements Flight {
+    public final Airport origin() {
+        return getLeg().origin();
+    }
+
+    public final Airport destination() {
+        return getLeg().destination();
+    }
+
+    public final LocalTime departureTime() {
+        return getFlightSchedule().departureTime();
+    }
+
+    public final LocalTime arrivalTime() {
+        return getFlightSchedule().arrivalTime();
+    }
+
+    public final boolean isShort(Duration durationMax) {
+        return getFlightSchedule().isShort(durationMax);
+    }
 }
