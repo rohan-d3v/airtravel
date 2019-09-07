11d65098bd254c7ca41e248b0a85b0ac20403b3a
Caitlin Campbell <cac226@case.edu>
2019-09-07 16:12:21 -0400

    added some things

---
+++ Airport/src/AbstractFlight.java
@@ -0,0 +1,3 @@
+public abstract class AbstractFlight implements Flight {
+
+}
--- Airport/src/Flight.java
+++ Airport/src/Flight.java
@@ -7,12 +7,12 @@
 import java.time.LocalTime;
 
 public interface Flight {
-    public String getCode();
-    public Leg getLeg();
-    public Airport origin();
-    public Airport destination();
-    public FlightSchedule getFlightSchedule();
-    public LocalTime departureTime();
-    public LocalTime arrivalTime();
-    public boolean isShort();
+    String getCode();
+    Leg getLeg();
+    Airport origin();
+    Airport destination();
+    FlightSchedule getFlightSchedule();
+    LocalTime departureTime();
+    LocalTime arrivalTime();
+    boolean isShort();
 }
