7a8142c73bbb11b852cddc30097d0b8257a39a4b
Rohan <rxr353@case.edu>
2019-09-07 16:27:19 -0400

    SimpleFlight Methods updated

--- Airport/src/SimpleFlight.java
+++ Airport/src/SimpleFlight.java
@@ -1,3 +1,4 @@
+import java.time.Duration;
 import java.time.LocalTime;
 
 /**
@@ -6,6 +7,31 @@ import java.time.LocalTime;
  * @project:    2) AirTravel
  * @class:      SimpleFlight
  */
-public final class SimpleFlight{
+public final class SimpleFlight extends AbstractFlight{
+
+    private final String code;
+    private final Leg leg;
+    private final FlightSchedule flightSchedule;
+
+    public SimpleFlight(String code, Leg leg, FlightSchedule flightSchedule) {
+        this.code = code;
+        this.leg = leg;
+        this.flightSchedule = flightSchedule;
+    }
+
+    @Override
+    public String getCode() {
+        return code;
+    }
+
+    @Override
+    public Leg getLeg() {
+        return null;
+    }
+    @Override
+    public FlightSchedule getFlightSchedule() {
+        return null;
+    }
+
 
 }
