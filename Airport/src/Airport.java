a2272e4fa14ed5497407ad9fb460b0df8cd1368b
Rohan <rxr353@case.edu>
2019-09-07 15:36:14 -0400

    Airport getter and override methods

--- Airport/src/Airport.java
+++ Airport/src/Airport.java
@@ -3,13 +3,32 @@ import java.time.Duration;
 //rohan and caitlin
 public  final class Airport {
 
-
+    String shortCode;
     String code;
     Duration ConnectionTimeMin;
 
     private Airport(String code, Duration connectionTimeMin) {
         code = getCode();
         ConnectionTimeMin = getConnectionTimeMin();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public String toString() {
+        return getShortCode();
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    public String getShortCode() {
+        return shortCode;
     }
 
     public String getCode() {
