c9af4d869f7e730f7f7f5d9ee22aef9e9cee7e5d
Caitlin Campbell <cac226@case.edu>
2019-09-13 13:36:56 -0400

    updating flight groups

--- Airport/src/FlightGroups.java
+++ Airport/src/FlightGroups.java
@@ -1,7 +1,9 @@
 import java.time.LocalTime;
-import java.util.HashSet;
 import java.util.NavigableMap;
+import java.util.TreeMap;
 import java.util.Set;
+import java.util.HashSet;
+import java.util.SortedMap;
 
 /**
  * @names:      Caitlin Campbell, Rohan Krishna Ramkhumar
@@ -12,34 +14,51 @@ import java.util.Set;
 public final class FlightGroups {
 
     private final Airport origin;
-    private final NavigableMap< LocalTime, Set<Flight>> flights;
+    private final NavigableMap<LocalTime, Set<Flight>> flights;
 
-    private FlightGroups(Airport origin, NavigableMap<LocalTime, Set<Flight>> flights) {
+    private FlightGroups(Airport origin) {
         this.origin = origin;
-        this.flights = flights;
+        this.flights = new TreeMap<LocalTime, Set<Flight>>();
+    }
+
+    public static final FlightGroups of(Airport origin)
+    {
+        if(origin == null)
+        {
+            throw new IllegalArgumentException("Origin airport cannot be null!");
+        }
+        return new FlightGroups(origin);
+    }
+
+    public final Airport getOrigin()
+    {
+        return origin;
     }
 
     public final boolean add(Flight flight){
         if(flights.containsValue(flight)) {
             throw new IllegalArgumentException("Flight already in flight group!");
         }
-        if(flights.containsKey(flight.getFlightSchedule().arrivalTime())) {
-            Set tempFlights = flights.get(flight.getFlightSchedule().arrivalTime());
-            tempFlights.add(flight);
-            flights.put(flight.getFlightSchedule().arrivalTime(), tempFlights);
-            return true;
+        if(flights.containsKey(flight.getFlightSchedule().departureTime())) {
+            Set tempFlights = flights.get(flight.getFlightSchedule().departureTime());
+            return tempFlights.add(flight);
+            //flights.put(flight.getFlightSchedule().departureTime(), tempFlights);
         }
-        flights.put(flight.getFlightSchedule().arrivalTime(), new HashSet<Flight>(java.util.Arrays.asList(flight)));
+        flights.put(flight.getFlightSchedule().departureTime(), new HashSet<Flight>(java.util.Arrays.asList(flight)));
         return true;
     }
 
     public final boolean remove(Flight flight){
-        return false;
+        return flights.remove(flight.getFlightSchedule().departureTime()) != null;
     }
-/*
-    public final Set<Flight> flightsAtOrAfter(LocalTime departureTime){
-        return
-    }
-    */
 
+    public final Set<Flight> flightsAtOrAfter(LocalTime departureTime){
+        SortedMap<LocalTime, Set<Flight>> atOrAfter = flights.tailMap(departureTime);
+        Set<Flight> result = new HashSet<Flight>();
+        for(Set<Flight> flightsList : atOrAfter.values())
+        {
+            result.addAll(flightsList);
+        }
+        return result;
+    }
 }
