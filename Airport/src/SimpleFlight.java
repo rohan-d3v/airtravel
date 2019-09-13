26a00552b459fc8910cb5cecc9ca9953a2fdb5ce
Caitlin Campbell <cac226@case.edu>
2019-09-13 13:44:10 -0400

    updated simpleflight

--- Airport/src/SimpleFlight.java
+++ Airport/src/SimpleFlight.java
@@ -10,10 +10,19 @@ public final class SimpleFlight extends AbstractFlight{
     private final Leg leg;
     private final FlightSchedule flightSchedule;
 
-    public SimpleFlight(String code, Leg leg, FlightSchedule flightSchedule) {
+    private SimpleFlight(String code, Leg leg, FlightSchedule flightSchedule) {
         this.code = code;
         this.leg = leg;
         this.flightSchedule = flightSchedule;
+    }
+
+    public static final SimpleFlight of(String code, Leg leg, FlightSchedule flightSchedule)
+    {
+        if(code == null || leg == null || flightSchedule == null)
+        {
+            throw new IllegalArgumentException("Cannot initialize SimpleFlight without a code, leg, and a flight schedule");
+        }
+        return new SimpleFlight(code, leg, flightSchedule);
     }
 
     @Override
