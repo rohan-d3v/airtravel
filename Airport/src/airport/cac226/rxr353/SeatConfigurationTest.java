9dd1cabf1da569aeab01401a0f956cb060b996dd
Rohan Krishna Ramkhumar <rxr353@case.edu>
2019-09-18 13:30:19 -0400

    SeatConfiguration Tests

--- Airport/src/airport/cac226/rxr353/SeatConfigurationTest.java
+++ Airport/src/airport/cac226/rxr353/SeatConfigurationTest.java
@@ -2,14 +2,33 @@ package airport.cac226.rxr353;
 
 import org.junit.Test;
 
+import java.util.EnumMap;
+
+import static airport.cac226.rxr353.SeatClass.BUSINESS;
+import static airport.cac226.rxr353.SeatClass.ECONOMY;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
 public class SeatConfigurationTest {
+
+    EnumMap<SeatClass, Integer> seatEnum = new EnumMap<SeatClass, Integer>(SeatClass.class);
+    public void setSeatEnum(){
+        seatEnum.put(BUSINESS, 2);
+    }
+
+    SeatConfiguration initSeatConfig = SeatConfiguration.of(seatEnum);
+    SeatConfiguration testSeatConfig = SeatConfiguration.of(initSeatConfig);
+
 
     @Test
     public void seats() {
+        assertEquals(2, testSeatConfig.seats(BUSINESS));
+        assertNotEquals(1, testSeatConfig.seats(BUSINESS));
     }
 
     @Test
     public void setSeats() {
+        assertEquals( 15, testSeatConfig.setSeats(ECONOMY, 15));
     }
 
     @Test
