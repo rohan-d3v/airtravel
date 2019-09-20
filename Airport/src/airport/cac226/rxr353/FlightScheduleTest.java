bed4bb43d10a12f06ff5361af5a2bdd3a6b90732
Rohan_home <rxr353@case.edu>
2019-09-20 12:42:36 -0400

    Incomplete tests, will continue working at office hours

--- Airport/src/airport/cac226/rxr353/FlightScheduleTest.java
+++ Airport/src/airport/cac226/rxr353/FlightScheduleTest.java
@@ -2,18 +2,23 @@ package airport.cac226.rxr353;
 
 import org.junit.Test;
 
+import java.time.Duration;
+import java.time.LocalTime;
+import java.time.ZoneId;
+
+import static org.junit.Assert.assertTrue;
+
+
 public class FlightScheduleTest {
 
 
+    Duration testDuration = Duration.ofHours(10);
+
+    FlightSchedule FS = FlightSchedule.of(LocalTime.now(), LocalTime.now(ZoneId.of("GMT")));
+
     @Test
     public void testIsShort() {
+        assertTrue(FS.isShort(testDuration));
     }
 
-    @Test
-    public void testDepartureTime() {
-    }
-
-    @Test
-    public void testArrivalTime() {
-    }
 }
